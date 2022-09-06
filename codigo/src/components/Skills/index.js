c6b24be541437c3846b7fe68893c6b37f36bc732
Gabrielttbr <ttbr2888@gmail.com>
2022-09-05 21:57:02 -0300

    Skills section finished

--- codigo/src/components/Skills/index.js
+++ codigo/src/components/Skills/index.js
@@ -1,16 +1,71 @@
 import * as S from "./styled";
 import TitleSection from "../TitleSection";
 
+import HTML from "../../assets/HTML.png";
+import CSS from "../../assets/CSS.png";
+import JS from "../../assets/JS-TEXT.png";
+import REACT from "../../assets/React-icon.svg (1).png";
+import TS from "../../assets/TS.png";
+import NEST from "../../assets/NEST-ICON.png";
+import NODE from "../../assets/NODEJS.png";
+import EXPRESS from "../../assets/EXPRESSs.png";
+import MYSQL from "../../assets/MYSQL-NOT-FOUND.png";
+import GIT from "../../assets/GIT-ICON.png";
+import GITHUB from "../../assets/GITHUB.png";
+import DOCKER from "../../assets/DOKCER.png";
+
 const Skills = () => {
+  const skillsObj = [
+    {
+      title: "FRONT-END ",
+      Skills: [
+        { descripition: "HTML", image: HTML },
+        { descripition: "CSS", image: CSS },
+        { descripition: "JAVASCRIPT", image: JS },
+        { descripition: "TYPESCRIPT", image: TS },
+        { descripition: "REACT", image: REACT },
+      ],
+    },
+    {
+      title: "BACK-END",
+      Skills: [
+        { descripition: "NEST", image: NEST },
+        { descripition: "NODE", image: NODE },
+        { descripition: "EXPRESS", image: EXPRESS },
+        { descripition: "MYSQL", image: MYSQL },
+      ],
+    },
+    {
+      title: "OUTROS",
+      Skills: [
+        { descripition: "GIT", image: GIT },
+        { descripition: "GITHUB", image: GITHUB },
+        { descripition: "DOCKER", image: DOCKER },
+      ],
+    },
+  ];
+  const returnSkills = (skills) => {
+    return skills.map((item, index) => (
+      <S.SkillItem key={index}>
+        <S.itemTitle>
+          <h1>{item.title}</h1>
+        </S.itemTitle>
+        <S.WrapperItens>
+        {item.Skills.map((item, index) => (
+          <S.item key={index}>
+              <img src={item.image} alt={item.descripition}></img>
+              <p>{item.descripition}</p>
+            </S.item>
+        ))}
+        </S.WrapperItens>
+      </S.SkillItem>
+    ));
+  };
   return (
     <S.Container>
       <S.ContainerChild>
         <TitleSection titleWhite="HABILI" titleOrage="DADES"></TitleSection>
-        <S.SkillSet>
-            <S.SkillItem></S.SkillItem>
-            <S.SkillItem></S.SkillItem>
-            <S.SkillItem></S.SkillItem>
-        </S.SkillSet>
+        <S.SkillSet>{returnSkills(skillsObj)}</S.SkillSet>
       </S.ContainerChild>
     </S.Container>
   );
--- codigo/src/components/Skills/styled.js
+++ codigo/src/components/Skills/styled.js
@@ -11,17 +11,91 @@ export const ContainerChild = styled.section`
     margin: auto;
 `
 export const SkillSet = styled.div`
-    margin: 20px;
+    margin: 10px;
 
 `
 export const SkillItem = styled.div`
     border: 1px solid var(--primary-color);
-    height: 25vh;
     width: 100%;
     margin-bottom: 40px;
+    display: flex;
+    align-items: center;
+    @media screen and (max-width: 769px){
+       flex-direction: column;
+    }
+    
+    @media screen and(max-width: 538px){
+        height: 100vh;
+    }
+`
+export const WrapperItens = styled.div`
+
+    width: 100%;
+    height:100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    @media screen and (max-width: 540px){
+       flex-direction: column;
+    }
+    
+    
+    
 `
 export const itemTitle = styled.div`
-    border-right: 3px solid var(--primary-color);
-    width: 16.6%;
-    height: 95%;
+    border-right: 6px solid   var(--primary-color);
+    width: 100% !important;
+    height: 80%;
+    padding: 20px;
+    display: flex;
+    justify-content:center;
+    align-items: center;
+    max-width: 200px;
+    
+    h1{
+        color: #fff;
+        font-weight:900;
+        font-size: 25px;
+    }
+    @media screen and (max-width: 769px){
+        border:none;
+        height: auto;
+        border-bottom: 6px solid  var(--primary-color);
+    }
+ 
+    
+`
+export const item = styled.div`
+    max-width: 158px;
+    width:20%;
+    margin: 10px;
+    display:flex;
+    flex-direction: column ;
+    justify-content:center;
+    align-items: center;
+
+    height: 80%;
+    p {
+        color: #Fff;
+        font-weight:900;
+        margin: 10px ;
+    }
+    img{
+    
+        height: 90px;
+    }
+
+    @media screen and (max-width: 540px){
+        max-width:100%;
+        width:100%;
+       img{
+        width: 40%;
+        height:auto ;
+       }
+       p{
+           font-size: 20px;
+       }
+    }
+    
+    
 `
